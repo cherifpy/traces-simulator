0168646afec630941deea5fbec2805cd85a0d812
cherifpy <simocherif2001@gmail.com>
2024-08-09 14:36:20 +0200

    fogrot 's'

--- cache-exp/exp/params.py
+++ cache-exp/exp/params.py
@@ -5,4 +5,4 @@ SERVER_REPLICA_MANAGER_PORT= 3030
 REP_PORT = 8080
 MEMCACHED_LISTENING_PORT = 11211
 PATH_TO_TASKS ="/home/csimohammed/exp/cache-exp/exp/traces/traces_with_datasets.csv" 
-PATH_TO_CONFIG_FILE = "/home/csimohammed/exp/cache-exp/configuration/conf.yaml"
+PATH_TO_CONFIG_FILE = "/home/csimohammed/exp/cache-exp/configurations/conf.yaml"
