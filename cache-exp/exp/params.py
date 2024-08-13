028143450e81fbd61628a32a91ddc7efaa4f9e4c
cherifpy <simocherif2001@gmail.com>
2024-08-13 16:26:13 +0200

    lets test on grid

--- cache-exp/exp/params.py
+++ cache-exp/exp/params.py
@@ -5,4 +5,4 @@ SERVER_REPLICA_MANAGER_PORT= 3030
 REP_PORT = 8880
 MEMCACHED_LISTENING_PORT = 11211
 PATH_TO_TASKS ="/home/csimohammed/exp/cache-exp/exp/traces/random_subset.csv" 
-PATH_TO_CONFIG_FILE = "/Users/cherif/Documents/Traveaux/traces-simulator/cache-exp/configurations/conf.yaml"#"/home/csimohammed/exp/cache-exp/configurations/conf.yaml"
+PATH_TO_CONFIG_FILE = "/home/csimohammed/exp/cache-exp/configurations/conf.yaml" #"/Users/cherif/Documents/Traveaux/traces-simulator/cache-exp/configurations/conf.yaml"
