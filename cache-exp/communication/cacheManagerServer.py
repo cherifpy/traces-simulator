ef006d53cb1db1de298fb3c55435a90e1ba6574f
cherifpy <simocherif2001@gmail.com>
2024-08-23 21:45:44 +0200

    test

--- cache-exp/communication/cacheManagerServer.py
+++ cache-exp/communication/cacheManagerServer.py
@@ -37,7 +37,7 @@ class CacheManagerServer:
 
             task = Task.from_json(data["task"])
             #{"task": data["task"], "type":data["type"] ,"status": "processed"}
-            self.recieved_task.put(task)
+            #self.recieved_task.put(task)
             self.writeOutput("test-1")
             b1 = self.cache.checkOnCacheMemorie(task.id_dataset)
             self.writeOutput("test")
